8bed7f26f527cc41d1d8cb93bef20288b4a00958
fairywellos <fairybutterfly73@gmail.com>
2019-03-09 12:55:26 +0000

    add missing close bracket

--- django_hasura/settings/settings.py
+++ django_hasura/settings/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 AUTH_USER_MODEL = 'users.User'
 AUTHENTICATION_BACKENDS = ['users.backends.EmailBackend']
