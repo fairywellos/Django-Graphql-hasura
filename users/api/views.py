f375eee93d5650f65d7c83a7a7f24c8e85e0e32c
fairywellos <fairybutterfly73@gmail.com>
2019-03-19 17:01:35 +0100

    make users endpoint open

--- users/api/views.py
+++ users/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.api.serializers import UserSerializer
@@ -5,6 +6,8 @@ from users.models import UserProxy as User
 
 
 class UserViewSet(ModelViewSet):
+    permission_classes = (AllowAny,)
+
     def get_queryset(self):
         return User.objects.all()
 
