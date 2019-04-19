6f490ed0e69b9620f42e663202f554fcd72a8075
fairywellos <fairybutterfly73@gmail.com>
2019-04-19 15:11:06 +0100

    fix client url bug

--- clients/utils.py
+++ clients/utils.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 def generate_client_url(subdomain):
     if settings.IS_GAE:
-        return f'{subdomain}-dot-f{settings.MAIN_DOMAIN_URL}'
+        return f'{subdomain}-dot-{settings.MAIN_DOMAIN_URL}'
     return f'{subdomain}.{settings.MAIN_DOMAIN_URL}'
