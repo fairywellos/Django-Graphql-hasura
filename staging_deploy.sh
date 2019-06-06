98b7dde92b46e753c5081b65a813cbdd0c720595
fairywellos <fairybutterfly73@gmail.com>
2019-06-06 11:24:30 +0000

    Update staging_deploy.sh

--- staging_deploy.sh
+++ staging_deploy.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-sudo su
-sudo chown -R $USER .
-git pull
-docker-compose down -v --rmi all --remove-orphans
+sudo git pull
+sudo docker-compose down -v --rmi all --remove-orphans
